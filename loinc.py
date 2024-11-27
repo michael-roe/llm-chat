036b7090483ae13a3f24ddcca473a5540747cf87
Michael Roe <michael-roe@users.noreply.github.com>
2024-11-27 21:55:01 +0000

    Add constants from the LOINC standard.

---
+++ loinc.py
@@ -0,0 +1,27 @@
+#
+# LOINC (loinc.com) defines standardized codes for use in healthcare
+# information processing systems
+
+#
+# Units of Measure
+#
+
+loinc_percent = "%"
+loinc_celcius = "Cel"
+loinc_centimetres = "cm"
+loinc_per_minute = "/min"
+loinc_millimetres_mercury = "mm[Hg]"
+loinc_kilograms = "kg"
+
+#
+# Vital Signs Panel
+#
+
+loinc_oxygen_saturation = "59408-5"
+loinc_systolic_blood_pressure = "8480-6"
+loinc_diastolic_blood_pressure = "8462-4"
+loinc_body_height = "8302-2"
+loinc_body_temperature = "8310-5"
+loinc_body_weight = "29463-7"
+loinc_heart_rate = "8867-4"
+loinc_respiratory_rate = "9279-1"
