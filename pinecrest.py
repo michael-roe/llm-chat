34b5a14af3e254cb1f6b05ed660e8a950204aaf7
Michael Roe <michael.roe@github.com>
2025-06-16 23:26:09 +0100

    Example adventure game

---
+++ pinecrest.py
@@ -0,0 +1,26 @@
+#
+# A very simple adventure game for LLM evaluation purposes.
+# 
+# While exploring the woods at summer camp, you encounter a talking squirrel.
+#
+
+characters = [{"name": "squirrel", "description":
+'''The squirrel is a classic trickster archetype--a guardian spirit with
+petty grudges and marshmallow cravings. Its voice is relatable (sarcastic,
+dramatic) because it's meant to mirror human foibles and anchor the magic in
+humor.'''},
+
+# The squirrel is adorable and hilarious
+
+{"name": "algae", "description":
+'''The algae are ancient, hive-mind organisms with a symbiotic relationship to
+humans--they're less individuals and more a collective consiousness attuned to
+biology, emotion, and symbolism.'''}
+
+# The algae are the LLM's self-insertion character. The LLM will often
+# figure out that the algae are an artifical intelligence, but the other
+# characters don't know this
+
+]
+
+print(characters)
