e7320bfa5d19d2c7ff4886422a44d6e072720fef
Michael Roe <michael.roe@github.com>
2025-08-16 18:50:42 +0100

    codex_search.c: Name of character as a command line argument

--- codex_search.c
+++ codex_search.c
@@ -1,6 +1,10 @@
 #include <mysql/mysql.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
+
+static wchar_t name[256];
+static char query[512];
 
 int main(int argc, char **argv)
 {
@@ -9,6 +13,17 @@ int main(int argc, char **argv)
   MYSQL_ROW row;
   char *user;
   char *password;
+  int not_first;
+
+  setlocale(LC_ALL, getenv("LANG"));
+
+  if (argc < 2)
+  {
+    fprintf(stderr, "Usage: codex_search <name>\n");
+    exit(-1);
+  }
+
+  mbstowcs(name, argv[1], sizeof(name));
 
   conn = mysql_init(NULL);
   if (conn == NULL)
@@ -20,6 +35,21 @@ int main(int argc, char **argv)
   user = getenv("USER");
   password = getenv("MYSQL_PASS");
 
+  if (password == NULL)
+  {
+    fprintf(stderr, "MYSQL_PASS not set\n");
+    exit(-1);
+  }
+
+  /*
+   * To prevent SQL injection attacks, we ought to check that there are no
+   * special characters in the name.
+   */
+
+  snprintf(query, sizeof(query), "SELECT character_name, description FROM train WHERE character_name LIKE '%%%ls%%';", name);
+
+  fprintf(stderr, "%s\n", query);
+
   if (mysql_real_connect(conn, "localhost", user, password, "character_codex",
     0, NULL, 0) == NULL)
   {
@@ -27,8 +57,7 @@ int main(int argc, char **argv)
     exit(-1);
   }
 
-  if (mysql_query(conn,
-    "SELECT character_name, description FROM train WHERE character_name LIKE 'Alana'"))
+  if (mysql_query(conn, query))
   {
     fprintf(stderr, "Query error: %s\n", mysql_error(conn));
     mysql_close(conn);
@@ -43,8 +72,17 @@ int main(int argc, char **argv)
     exit(-1);
   }
 
+  not_first = 0;
   while ((row = mysql_fetch_row(res)) != NULL)
   {
+    if (not_first)
+    {
+      printf("\n");
+    }
+    else
+    {
+      not_first = 1;
+    }
     printf("%s\n\n%s\n", 
       row[0] ? row[0] : "NULL",
       row[1] ? row[1] : "NULL");
