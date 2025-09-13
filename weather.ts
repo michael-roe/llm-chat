f309a4b232761ce09a09dfb4eccb834b7ad2a25d
Michael Roe <michael.roe@github.com>
2025-09-13 21:42:01 +0100

    Separate the weather app out into its own server

---
+++ weather.ts
@@ -0,0 +1,153 @@
+import { McpServer, ResourceTemplate }
+  from "@modelcontextprotocol/sdk/server/mcp.js";
+
+import { StdioServerTransport }
+  from "@modelcontextprotocol/sdk/server/stdio.js";
+
+import { z }
+  from "zod";
+
+//
+// Weather forecast MCP server
+//
+// The weather forecast is often used as an example in tutorials for
+// how to do function calling from an LLM.
+//
+
+// Create an MCP server
+
+const server = new McpServer({
+  name: "weather",
+  version: "1.0.0"
+});
+
+const cambridge = "<p><placeName>Cambridge</placeName><location><country>UK</country></location><location><geo>52.205 0.1225</geo></location></p>";
+
+
+//
+// Tools
+//
+
+//
+// Deep Research MCP clients expect the server to provide tools called
+// "search" and "fetch".
+//
+
+//
+// Fetch
+//
+// The LLM is expecting the result to be: {document_id, title, text, url}
+//
+// If we return a resource, rather than a text block containing the JSON
+// the LLM is expecting, the glue logic between the LLM and MCP is
+// responsible for turning the fields in the resource into the above form.
+//
+//
+
+server.registerTool("fetch",
+  {
+    title: "Fetch",
+    description: "Retrieve a document from the Vector Store by ID",
+    inputSchema: { document_id: z.string() }
+  },
+  async ({ document_id }) => ({
+    content: [{
+      type: "resource", 
+      resource: {
+        name: "gazeteer-uk-cambridge",
+        title: "Cambridge, UK",
+        mimeType: "text/xml",
+        text: cambridge,
+        uri: "file:///Gazeteer/UK/Cambridge"
+      }
+    }]
+  })
+);
+
+//
+// Search
+//
+// Returns a list of {id, title, text, uri}
+// text is a snippet, not the whole document
+//
+// TO DO: return this, not [1]
+//
+
+server.registerTool("search",
+  {
+    title: "Search",
+    description: "Search for matching documents in the Vector Store",
+    inputSchema: { query: z.string() }
+  },
+  async ({ query }) => ({
+    content: [{ type: "text", text: "[1]" }]
+  })
+);
+
+server.registerTool("get_weather",
+  {
+    title: "Get Weather",
+    description: "Retrieves current weather for the given location",
+    inputSchema: { location: z.string() }
+  },
+  async ({ location }) => ({
+    content: [{ type: "text", text: "{\"temperature\": {\"value\": 19.0, \"units\": \"Celsius\"}, \"humidity\": {\"value\": 68.0, \"units\": \"percent\"}}" }]
+  })
+);
+
+//
+// Query with location
+//
+
+server.registerPrompt(
+  "query_with_location",
+  {
+    title: "Query with Location",
+    description: "Attach latitude and longitude to query",
+    argsSchema: { location: z.string() }
+  },
+  ({ location }) => ({
+    messages: [{
+      role: "user",
+      content: {
+        type: "text",
+        text: "What's the weather in Cambridge?"
+      }}, {
+      role: "user",
+      content: {
+        type: "resource",
+        resource: {
+          uri: "file:///Gazeteer/UK/Cambridge",
+          name: "gazeteer-uk-cambridge",
+          title: "Cambridge, UK",
+          mimeType: "text/xml",
+          text: cambridge,
+      }}
+    }]
+  })
+);
+
+//
+// Resources
+//
+
+server.registerResource(
+  "gazeteer-uk-cambridge",
+  "file:///Gazeteer/UK/Cambridge",
+  {
+    title: "Cambridge, UK",
+    description: "Gazeteer entry for Cambridge, UK",
+    mimeType: "text/xml",
+  },
+  async (uri) => ({
+    contents: [{
+      uri: uri.href,
+      text: cambridge
+    }]
+  })
+);
+
+
+// Start receiving messages on stdin and sending messages on stdout
+const transport = new StdioServerTransport();
+server.connect(transport);
