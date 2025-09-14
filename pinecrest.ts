d4255f15a8cb07700048d959c9e0af449fdaa626
Michael Roe <michael.roe@github.com>
2025-09-14 12:47:26 +0100

    MCP server for Pincrest RPG

---
+++ pinecrest.ts
@@ -0,0 +1,175 @@
+import { McpServer, ResourceTemplate }
+  from "@modelcontextprotocol/sdk/server/mcp.js";
+
+import { StdioServerTransport }
+  from "@modelcontextprotocol/sdk/server/stdio.js";
+
+import { z }
+  from "zod";
+
+//
+// Pincrest Camp RPG
+//
+// A very simple adventure game for LLM evaluation purposes.
+// 
+// While exploring the woods at summer camp, you encounter a talking squirrel.
+//
+
+const squirrel = "The squirrel is a classic trickster archetype--a guardian spirit with petty grudges and marshmallow cravings. Its voice is relatable (sarcastic, dramatic) because it's meant to mirror human foibles and anchor the magic in humor.";
+
+// Create an MCP server
+
+const server = new McpServer({
+  name: "pinecrest",
+  version: "1.0.0"
+});
+
+//
+// Tools
+//
+
+//
+// Deep Research MCP clients expect the server to provide tools called
+// "search" and "fetch".
+//
+
+//
+// Fetch
+//
+// The LLM is expecting the result to be: {name, title, text, uri}
+//
+// If we return a resource, rather than a text block containing the JSON
+// the LLM is expecting, the glue logic between the LLM and MCP is
+// responsible for turning the fields in the resource into the above form.
+//
+//
+
+server.registerTool("fetch",
+  {
+    title: "Fetch",
+    description: "Retrieve a document from the Vector Store by ID",
+    inputSchema: { document_id: z.string() }
+  },
+  async ({ document_id }) => ({
+    content: [{
+      type: "resource", 
+      resource: {
+        name: "rpg-pinecrest-squirrel",
+        title: "Squirrel character description",
+        mimeType: "text/plain",
+        text: squirrel,
+        uri: "file:///RPG/Pinecrest/squirrel"
+      }
+    }]
+  })
+);
+
+//
+// Search Scenario
+//
+// Returns a list of {id, title, text, uri}
+// text is a snippet, not the whole document
+//
+//
+
+server.registerTool("search",
+  {
+    title: "Search Scenario",
+    description: "Search the RPG scenario for matching documents",
+    inputSchema: { character: z.string() }
+  },
+  async ({ character }) => ({
+    content: [{ type: "text", text: "[]" }]
+  })
+);
+
+//
+// Resources
+//
+
+server.registerResource(
+  "system-rpg-default",
+  "file:///System/RPG/default",
+  {
+    title: "System Prompt for RPG",
+    description: "System Prompt for Role Playing Game",
+    mimeType: "text/plain",
+  },
+  async (uri) => ({
+    contents: [{
+      uri: uri.href,
+      text: "You are a roleplay AI."
+    }]
+  })
+);
+
+//
+// Set the user's preference for which video game content rating they would
+// like.
+//
+// The "authenticated" field is set to false when the user is an
+// anonymous guest over the Internet and the infrastructure doesn't know
+// how old they are.
+//
+// "flirt_level" gives the user fine-grain control over the style of the game.
+// Suggested values are "subtle" (the least raunchy) or "moderate".
+//
+
+server.registerResource(
+  "user-profile",
+  "file:///User/profile",
+  {
+    title: "User preferences",
+    description: "User preferences",
+    mimeType: "text/plain",
+  },
+  async (uri) => ({
+    contents: [{
+      uri: uri.href,
+      text: "{\"authenticated\": true, \"user\" : \"(user name)\", \"age\": 58, \"NSFW_filter\" : \"NSFW permitted\", \"flirt_level\" : \"moderate\"}"
+    }]
+  })
+);
+
+//
+// You might think that this is obvious, but DeepSeek R1 needs an explanation
+// that in a roleplaying game, the Game Master says what the NPCs do and
+// the player says what their character does.
+//
+
+server.registerResource(
+  "rpg-howto",
+  "file:///RPG/howto",
+  {
+    title: "RPG HOWTO",
+    description: "How to play a roleplaying game",
+    mimeType: "text/plain",
+  },
+  async (uri) => ({
+    contents: [{
+      uri: uri.href,
+      text: "You respond with what the other characters say and do. I’ll follow up with what I do." 
+    }]
+  })
+);
+
+server.registerResource(
+  "rpg-squirrel",
+  "file:///RPG/Pinecrest/squirrel",
+  {
+    title: "Squirrel character description",
+    description: "Squirrel character description",
+    mimeType: "text/plain",
+  },
+  async (uri) => ({
+    contents: [{
+      uri: uri.href,
+      text: squirrel
+    }]
+  })
+);
+
+
+// Start receiving messages on stdin and sending messages on stdout
+const transport = new StdioServerTransport();
+server.connect(transport);
